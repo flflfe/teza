14ca6c9f6dfc0c046ba23db5a30cbd809ccc3249
Souvik Biswas <sbis1999@gmail.com>
2021-06-27 14:28:11 +0530

    Add dashbaord page

---
+++ lib/screens/dashboard_page.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+import 'package:teza/model/wallet.dart';
+import 'package:teza/res/palette.dart';
+
+class DashboardPage extends StatefulWidget {
+  final Wallet wallet;
+
+  const DashboardPage({@required this.wallet});
+
+  @override
+  _DashboardPageState createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+  Wallet _wallet;
+  String _amountString;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _wallet = widget.wallet;
+    _amountString = (double.parse(_wallet.amount) / 1000000).toStringAsFixed(2);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Palette.background,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Palette.background,
+      ),
+      drawer: Drawer(),
+      body: Padding(
+        padding: const EdgeInsets.only(
+          top: 8.0,
+          left: 16.0,
+          right: 16.0,
+        ),
+        child: Column(
+          children: [
+            Row(),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'amount',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Montserrat',
+                    fontWeight: FontWeight.w500,
+                    fontSize: 20.0,
+                    letterSpacing: 1,
+                  ),
+                ),
+                SizedBox(height: 8.0),
+                Text(
+                  _amountString,
+                  style: TextStyle(
+                    color: Palette.primary,
+                    fontFamily: 'Montserrat',
+                    fontWeight: FontWeight.w400,
+                    fontSize: 50.0,
+                    letterSpacing: 1,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
