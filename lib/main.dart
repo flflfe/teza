7671c8e7d0bebeb36d4f3fa1edb923f5a4b27844
Souvik Biswas <sbis1999@gmail.com>
2021-06-27 14:28:01 +0530

    Hive update

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:teza/model/wallet.dart';
 
 import 'screens/initial_page.dart';
 
-void main() {
+void main() async {
+  final appDocDir = await getApplicationDocumentsDirectory();
+  Hive.init(appDocDir.path);
+  Hive.registerAdapter(WalletAdapter());
   runApp(MyApp());
 }
 
