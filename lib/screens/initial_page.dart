fa9eec6d17af89dc0905ff698c1c87e81ea21975
Souvik Biswas <sbis1999@gmail.com>
2021-06-27 13:00:03 +0530

    Initial starting page update

--- lib/screens/initial_page.dart
+++ lib/screens/initial_page.dart
@@ -1,8 +1,134 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:teza/res/palette.dart';
+import 'package:teza/screens/generate_wallet_page.dart';
 
-class InitialPage extends StatelessWidget {
+class InitialPage extends StatefulWidget {
+  @override
+  _InitialPageState createState() => _InitialPageState();
+}
+
+class _InitialPageState extends State<InitialPage> {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Palette.background,
+      statusBarIconBrightness: Brightness.light,
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Palette.background,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 16.0,
+            right: 16.0,
+            bottom: 32.0,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(),
+              Expanded(
+                child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        'teza',
+                        style: TextStyle(
+                          color: Palette.primary,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 62,
+                        ),
+                      ),
+                      Text(
+                        'powered by Tezos',
+                        style: TextStyle(
+                          color: Palette.primary,
+                          fontWeight: FontWeight.w300,
+                          letterSpacing: 1,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 8.0,
+                  right: 8.0,
+                ),
+                child: Container(
+                  width: double.maxFinite,
+                  child: RaisedButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => GenerateWalletPage(),
+                        ),
+                      );
+                    },
+                    color: Palette.primary,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        top: 16.0,
+                        bottom: 16.0,
+                      ),
+                      child: Text(
+                        'Generate wallet',
+                        style: TextStyle(
+                          color: Palette.smokyBlack,
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.w600,
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16.0),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 8.0,
+                  right: 8.0,
+                ),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.download_rounded,
+                      color: Colors.white,
+                      size: 32.0,
+                    ),
+                    SizedBox(width: 8.0),
+                    Expanded(
+                      child: Text(
+                        'Already have a wallet? Click here to import',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.w400,
+                          fontSize: 14.0,
+                          letterSpacing: 1.2,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
