dbd9391954a71a0bb786d02e13f89d1b2b6a727b
Souvik Biswas <sbis1999@gmail.com>
2021-06-27 12:59:39 +0530

    Add color palette

---
+++ lib/res/palette.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class Palette {
+  static final Color primary = Color(0xFF80FF72);
+  static final Color background = Color(0xFF153243);
+  static final Color smokyBlack = Color(0xFF0F0A0A);
+}
